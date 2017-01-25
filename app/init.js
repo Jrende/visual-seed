ae2b40b972b3a65bc167148367478229eeef3c92
Johan Rende <johan@rende.se>
2017-01-25 20:08:05 +0100

    Let's use webgl 1.0 until I actually need 2.0

--- app/init.js
+++ app/init.js
@@ -1,5 +1,5 @@
 let canvas = document.querySelector('canvas');
-let context = canvas.getContext('webgl2');
+let context = canvas.getContext('webgl');
 if(context == null) {
   console.error('Unable to create WebGL 2.0 context');
 }
