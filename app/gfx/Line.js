c42fe9f17a525029329645d0ff44d1564e4d2344
Johan Rende <johan@rende.se>
2017-01-28 15:20:01 +0100

    Fixed lines

--- app/gfx/Line.js
+++ app/gfx/Line.js
@@ -1,19 +1,42 @@
 import * as glm from 'gl-matrix';
 import {degToRad} from './Utils.js';
 
+function calculateModelMatrix(line) {
+  let midpoint = [
+    (line.to[0]+line.from[0]),
+    (line.to[1]+line.from[1]),
+    (line.to[2]+line.from[2])
+  ].map(i => i/2.0);
+  let p = [
+    line.to[0] - line.from[0],
+    line.to[1] - line.from[1],
+    line.to[2] - line.from[2]
+  ]
+  let len = Math.sqrt(p[0] * p[0] + p[1] * p[1] + p[2] * p[2]);
+  let angle = -Math.atan(p[1]/p[0]);
+
+  glm.mat4.identity(line.mat);
+  glm.mat4.translate(line.mat, line.mat, midpoint);
+  glm.mat4.rotateZ(line.mat, line.mat, angle);
+  glm.mat4.scale(line.mat, line.mat, [line.width, len/2.0, 1]);
+}
+
 export default class Line {
-  constructor(from, to) {
+  constructor(from, to, width = 50) {
     this.from = from;
     this.to = to;
+    this.width = width;
     this.mat = glm.mat4.create();
-    /*
-    glm.mat4.fromScaling(this.mat, [100, 100, 100]);
-    glm.mat4.translate(this.mat, this.mat, [500, 500, 0]);
-    glm.mat4.rotateZ(this.mat, this.mat, degToRad(45));
-    */
+    if(from.length === 2) {
+      from[2] = 0.0;
+    }
+    if(to.length === 2) {
+      to[2] = 0.0;
+    }
+    calculateModelMatrix(this);
   }
 
   getModelMatrix() {
-    return this.mat;
+    return this.mat
   }
 }
--- app/main.js
+++ app/main.js
@@ -22,8 +22,8 @@ let projectionMatrix = glm.mat4.create();
 //glm.mat4.ortho(projectionMatrix, 1, -1, -1, 1, 0.1, 100);
 glm.mat4.ortho(projectionMatrix, width/2, -width/2, -height/2, height/2, 0.1, 100);
 //glm.mat4.perspective(projectionMatrix, 90, width/height, 0.1, 100);
-
-let line = new Line([0, 0], [1, 2]);
+window.to = [50, 50]
+let line = new Line([0, 0], window.to, 5);
 
 let modelMatrix = glm.mat4.create();
 //glm.mat4.rotateX(modelMatrix, modelMatrix, degToRad(180));
@@ -33,16 +33,32 @@ glm.mat4.translate(viewMatrix, viewMatrix, [0, 0, -10]);
   //glm.mat4.rotateY(modelMatrix, modelMatrix, 0.01);
 gl.disable(gl.CULL_FACE);
 gl.cullFace(gl.BACK);
+function drawPoint(x, y) {
+  let mvp2 = glm.mat4.create();
+  glm.mat4.multiply(mvp2, mvp2, projectionMatrix);
+  glm.mat4.multiply(mvp2, mvp2, viewMatrix);
+  let scale = glm.mat4.create();
+  glm.mat4.translate(scale, scale, [x, y, 0]);
+  glm.mat4.multiply(mvp2, mvp2, scale);
+
+  shader.solid.bind(gl);
+  shader.solid.uniforms.r = 1.0;
+  shader.solid.uniforms.g = 1.0;
+  shader.solid.uniforms.b = 1.0;
+  shader.solid.uniforms.alpha = 1.0;
+  shader.solid.uniforms.mvp = mvp2;
+  gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+
+}
 
 function render() {
   gl.clear(gl.COLOR_BUFFER_BIT);
+  quad.bind();
 
   let mvp = glm.mat4.create();
   glm.mat4.multiply(mvp, mvp, projectionMatrix);
   glm.mat4.multiply(mvp, mvp, viewMatrix);
-  //glm.mat4.rotateZ(modelMatrix, modelMatrix, 0.1);
-  //glm.mat4.rotateX(modelMatrix, modelMatrix, degToRad(2));
-  glm.mat4.multiply(mvp, mvp, modelMatrix);
+  glm.mat4.multiply(mvp, mvp, line.getModelMatrix());
 
   shader.solid.bind(gl);
   shader.solid.uniforms.r = 0.5;
@@ -50,8 +66,10 @@ function render() {
   shader.solid.uniforms.b = 1.5;
   shader.solid.uniforms.alpha = 1.0;
   shader.solid.uniforms.mvp = mvp;
-  quad.bind();
   gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+
+  drawPoint(window.to[0], window.to[1])
+  drawPoint(0, 0)
   window.requestAnimationFrame(render);
 }
 render();
